8a627a8f7535e63010b28a469b8dc23a519192b8
Tomachi Ajoku <mrtomachi@gmail.com>
2021-02-27 11:24:15 +0100

    added model structure

---
+++ src/main/java/com/interview/usermanagementsystem/enums/Role.java
@@ -0,0 +1,5 @@
+package com.interview.usermanagementsystem.enums;
+
+public enum Role {
+    USER, ADMIN
+}
---
+++ src/main/java/com/interview/usermanagementsystem/model/User.java
@@ -0,0 +1,73 @@
+package com.interview.usermanagementsystem.model;
+
+import com.interview.usermanagementsystem.enums.Role;
+import com.interview.usermanagementsystem.enums.Status;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+import static javax.persistence.TemporalType.TIMESTAMP;
+
+@Data
+@Builder
+@Entity
+@Table(name = "user_tbl")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String firstname;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String mobile;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "registered_at", updatable = false)
+    private Date registeredAt;
+
+    @Column(nullable = false)
+    private boolean verified;
+
+    @Temporal(TIMESTAMP)
+    @Column(name = "verified_at")
+    private Date verifiedAt;
+
+    @Column(nullable = false)
+    private boolean active;
+
+    @Temporal(TIMESTAMP)
+    @Column(name = "deactivated_at")
+    private Date deactivatedAt;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status;
+}
